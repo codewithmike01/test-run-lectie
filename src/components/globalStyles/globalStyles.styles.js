cb9f4225d9bd8856539cda00779002eaf1723254
ginohmk <michotall95@gmail.com>
2022-10-14 17:51:30 +0100

    Add: Typography as in the figma, h1,h2,h3,h4,h5

---
+++ src/components/globalStyles/globalStyles.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const primaryColor = '#EF6D02';
+export const midPrimaryColor = '#F38216';
+export const successColor = '#055D1D';
+export const warningColor = ' #9D8F14';
+export const errorColor = '#AA170D';
